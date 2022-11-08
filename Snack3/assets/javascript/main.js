cad4bf874aa78aee94e6a5f76564d1a11f5062f0
yurididomenico <yurididomenico93@gmail.com>
2022-11-08 14:11:53 +0100

    Set Workspace Snack 3

---
+++ Snack3/assets/javascript/main.js
@@ -0,0 +1,55 @@
+// JAVASCRIPT //
+
+console.log("YDD");
+
+
+/*
+
+-Creare un array di oggetti:
+    -Ogni oggetto descriverà una bici da corsa con le seguenti proprietà:
+        -nome
+        -peso
+Stampare a schermo la bici con peso minore utilizzando destructuring e template literal
+
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// ----- FINE :) -----
