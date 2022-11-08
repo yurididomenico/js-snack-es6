6ff66a4fd59996b2eb541f0cd799b9e8ec90dc6a
yurididomenico <yurididomenico93@gmail.com>
2022-11-08 15:34:17 +0100

    Set Workspace Snack 4

---
+++ Snack4/assets/javascript/main.js
@@ -0,0 +1,68 @@
+// JAVASCRIPT //
+
+console.log("YDD");
+
+
+/*
+
+Creare un array di oggetti di squadre di calcio.
+Ogni squadra avrà diverse proprietà:
+-nome
+-punti fatti
+-falli subiti
+    Nome sarà l’unica proprietà da compilare
+    le altre saranno tutte settate a 0.
+Generare numeri random al posto degli 0 nelle proprietà:
+-Punti fatti
+-falli subiti.
+Infine usando la destrutturazione creiamo un nuovo array i cui elementi contengono solo nomi e falli subiti
+stampiamo tutto in console.
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// console.log(biciLeggera)
+
+   
+
+
+
+// ----- FINE :) -----
